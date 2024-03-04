9853b0e867e351a2d3af6cafb4eb1f94fd9eac0a
alfiahayu <130026057+alfiahayu@users.noreply.github.com>
2024-03-04 21:32:32 +0700

    Update project_dashboard.py

--- project_dashboard.py
+++ project_dashboard.py
@@ -49,7 +49,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Load cleaned data
-all_df = pd.read_excel("all_data.xlsx")
+all_df = pd.read_csv("all_data.csv")
 
 datetime_columns = ["order_approved_at_x", "order_delivered_customer_date_x"]
 for column in datetime_columns:
